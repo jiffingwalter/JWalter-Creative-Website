88d38bfb2076e57255be5d79252f07c1a24079b8
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-12-13 18:45:18 -0500

    yeah

---
+++ src/app/ts/CommissionHandler.ts
@@ -0,0 +1,17 @@
+/** Commission handler class
+ * Utility functions for commission class objects
+*/
+import { Commission } from './CommissionClass';
+
+export class ComssionHandler{
+    /** Generates a mock commission with placeholder data */
+    getMock(){
+        let commission = new Commission(
+            'Doug Dimmadome',
+            'Mock commission',
+            new Date(),
+            'open'
+        );
+        return commission;
+    }
+}
