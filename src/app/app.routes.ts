1ce18a187f1446f23a257a0aeac559e8a246fcc6
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-12-19 22:15:29 -0500

    added routing and not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { GalleryComponent } from './components/pages/gallery/gallery.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { EtsyComponent } from './components/pages/etsy/etsy.component';
+import { NotfoundComponent } from './components/pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'gallery', component: GalleryComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'etsy', component: EtsyComponent},
+    {path: '**', component: NotfoundComponent, pathMatch: 'full'}
+];
--- src/app/components/jw-body/jw-body.component.ts
+++ src/app/components/jw-body/jw-body.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Commission } from '../../ts/CommissionClass';
 
 @Component({
     selector: 'app-jw-body',
     standalone: true,
-    imports: [],
+    imports: [
+        RouterOutlet
+    ],
     templateUrl: './jw-body.component.html',
     styleUrl: './jw-body.component.css',
 })
