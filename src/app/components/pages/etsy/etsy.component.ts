b1bf89e130ae5657cdcd9f9ec012f67def8232fa
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-12-23 17:03:18 -0500

    Added title service and routing css

--- src/app/components/pages/etsy/etsy.component.ts
+++ src/app/components/pages/etsy/etsy.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-etsy',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './etsy.component.css'
 })
 export class EtsyComponent {
-
+  constructor(private titleService: Title){
+    this.titleService.setTitle('Etsy');
+  }
 }
