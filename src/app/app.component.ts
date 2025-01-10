b77edba210939e3aec940f7e69b07feaf20de4db
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-01-09 22:42:48 -0500

    Added hardcoded landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { JwHeaderComponent } from './components/shared/jw-header/jw-header.component';
 import { JwBodyComponent } from './components/shared/jw-body/jw-body.component';
 import { JwFooterComponent } from './components/shared/jw-footer/jw-footer.component';
+import { LandingComponent } from './components/pages/landing/landing.component';
 
 @Component({
     selector: 'app-root',
@@ -14,10 +15,13 @@ import { JwFooterComponent } from './components/shared/jw-footer/jw-footer.compo
         JwHeaderComponent,
         JwBodyComponent,
         JwFooterComponent,
+        LandingComponent
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
     title = 'Angular-Sandbox';
+    
+    showLanding:Boolean = true;
 }
