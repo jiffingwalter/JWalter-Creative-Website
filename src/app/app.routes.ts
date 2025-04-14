a48f7798d4d75800df3180baab5d471c76015e98
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-04-13 21:28:51 -0400

    Switched routing process to lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,63 @@
 import { Routes } from '@angular/router';
-import { NotfoundComponent } from './components/pages/notfound/notfound.component';
-import { UnderconstructionComponent } from './components/pages/underconstruction/underconstruction.component';
-import { LandingComponent } from './components/pages/landing/landing.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { ArtComponent } from './components/pages/art/art.component';
-import { GamesComponent } from './components/pages/games/games.component';
-import { CodeComponent } from './components/pages/code/code.component';
+//import { NotfoundComponent } from './components/pages/notfound/notfound.component';
+//import { UnderconstructionComponent } from './components/pages/underconstruction/underconstruction.component';
+//import { LandingComponent } from './components/pages/landing/landing.component';
+//import { HomeComponent } from './components/pages/home/home.component';
+//import { ArtComponent } from './components/pages/art/art.component';
+//import { GamesComponent } from './components/pages/games/games.component';
+//import { CodeComponent } from './components/pages/code/code.component';
 
 /* TODO: set these all up for lazy loading */
-export const routes:Routes = [
-    {path: '', component: LandingComponent, title: "Welcome", data: {showNav: false}},
+export const routes: Routes = [
+    { path: '',
+        loadComponent: () => import('./components/pages/landing/landing.component').then(m => m.LandingComponent),
+        title: "Welcome",
+        data: { showNav: false } },
 
-    {path: 'home', component: HomeComponent, title: "Home", data: {showNav: true}},
-    {path: 'art', component: ArtComponent, title: "Art", data: {showNav: true}, children:[
-        {path: 'gallery', title: "Gallery", component: UnderconstructionComponent},
-        {path: 'avatar-creator', title: "Avatar Creator", component: UnderconstructionComponent}
-    ]},
-    {path: 'games', component: GamesComponent, title: "Games", data: {showNav: true}},
-    {path: 'code', component: CodeComponent, title: "Code", data: {showNav: true}},
+    { path: 'home',
+        loadComponent: () => import('./components/pages/home/home.component').then(m => m.HomeComponent),
+        title: "Home",
+        data: { showNav: true }
+    },
+    { path: 'art',
+        loadComponent: () => import('./components/pages/art/art.component').then(m => m.ArtComponent),
+        title: "Art",
+        data: { showNav: true },
+        children: [
+            { path: 'gallery', title: "Gallery", loadComponent: () => import('./components/pages/underconstruction/underconstruction.component').then(m => m.UnderconstructionComponent) },
+            { path: 'avatar-creator', title: "Avatar Creator", loadComponent: () => import('./components/pages/underconstruction/underconstruction.component').then(m => m.UnderconstructionComponent) }
+        ]
+    },
+    { path: 'games',
+        loadComponent: () => import('./components/pages/games/games.component').then(m => m.GamesComponent),
+        title: "Games",
+        data: { showNav: true }
+    },
+    { path: 'code',
+        loadComponent: () => import('./components/pages/code/code.component').then(m => m.CodeComponent),
+        title: "Code",
+        data: { showNav: true }
+    },
 
-    {path: '**', component: NotfoundComponent, pathMatch: 'full', title: "Page not found :(", data: {showNav: false}}
+    { path: '**',
+        loadComponent: () => import('./components/pages/notfound/notfound.component').then(m => m.NotfoundComponent),
+        pathMatch: 'full',
+        title: "Page not found :(",
+        data: { showNav: false }
+    }
 ];
 
 // Get nav buttons out of the router elements 
-export interface NavItem{
-    name: string|undefined,
-    path: string|undefined
+export interface NavItem {
+    name: string | undefined,
+    path: string | undefined
 };
 
-let navList:Array<NavItem> = [];
-for (let i in routes){
+let navList: Array<NavItem> = [];
+for (let i in routes) {
     const element = routes[i];
-    if (element.data && element.data['showNav'] === true){
-        const nav:NavItem = {
+    if (element.data && element.data['showNav'] === true) {
+        const nav: NavItem = {
             name: element.title?.toString(),
             path: element.path?.toString()
         }
@@ -40,4 +65,4 @@ for (let i in routes){
     }
 }
 
-export const navListExport:Array<NavItem> = navList;
+export const navListExport: Array<NavItem> = navList;
