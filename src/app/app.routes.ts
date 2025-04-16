977a7a63743593ea7599525559072e7170c08d31
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-04-15 22:53:50 -0400

    [dev] Css work on header/body

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,4 @@
 import { Routes } from '@angular/router';
-//import { NotfoundComponent } from './components/pages/notfound/notfound.component';
-//import { UnderconstructionComponent } from './components/pages/underconstruction/underconstruction.component';
-//import { LandingComponent } from './components/pages/landing/landing.component';
-//import { HomeComponent } from './components/pages/home/home.component';
-//import { ArtComponent } from './components/pages/art/art.component';
-//import { GamesComponent } from './components/pages/games/games.component';
-//import { CodeComponent } from './components/pages/code/code.component';
 
 /* TODO: set these all up for lazy loading */
 export const routes: Routes = [
