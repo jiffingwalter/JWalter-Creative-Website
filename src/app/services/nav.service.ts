167dc861902b35741b0b8599113c0f6bf1d76732
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-04-23 22:08:58 -0400

    [chore] Removed console log in nav service

--- src/app/services/nav.service.ts
+++ src/app/services/nav.service.ts
@@ -11,9 +11,9 @@ export class NavService {
 
   constructor(private router: Router) {
     this.routes = this.router.config;
+    
     for (let i in this.routes) {
       const element = this.routes[i];
-      console.log(element);
 
       if (element.data && element.data['showNav'] === true) {
         let navChildren: Array<NavItem> = [];
