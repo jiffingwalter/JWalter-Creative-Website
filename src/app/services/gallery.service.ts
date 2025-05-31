8bf89fb6074135f7b0bebe90f7c22b96684e71b2
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-05-31 18:47:34 -0400

    Setup environment files and variables

--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -2,18 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GalleryItem } from '@interfaces/gallery-item.interface';
 import { firstValueFrom } from 'rxjs';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GalleryService {
+  private api = environment.api;
 
   constructor(private http: HttpClient) {
   }
 
   async getGalleryItems(): Promise<GalleryItem[]> {
     return await firstValueFrom(
-      this.http.get<GalleryItem[]>('http://localhost:8080/api/v1/get-gallery-items')
+      this.http.get<GalleryItem[]>(this.api + '/v1/get-gallery-items')
     );
   }
 }
