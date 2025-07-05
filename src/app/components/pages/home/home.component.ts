e472ea7e1f053e8c78abf7f716ffa52af5534dc2
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-07-05 17:55:52 -0400

    Replaced skill placeholder text on Home, css adjustments

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,43 +14,43 @@ export class HomeComponent {
   skills: Array<SkillItem> = [
     {
       label: 'HTML/CSS',
-      level: 'Intermediate',
-      description: 'html css experience'
+      level: 'Knowledgeable',
+      description: 'Built and refactored reactive and responsive webpages with vanilla HTML and CSS from scratch, as well as taking advantage of different front-end frameworks such as Bootstrap. Focusing on learning more foundations on accessible webpage design.'
     },
     {
       label: 'JavaScript',
-      level: 'Intermediate',
-      description: 'javascript experience'
+      level: 'Knowledgeable',
+      description: 'Leveraged vanilla JavaScript for building interactive functionality into enterprise-level webpages and webapps, building and utilizing APIs, as well as for personal projects.'
     },
     {
       label: 'TypeScript',
-      level: 'Intermediate',
-      description: 'typescript experience'
+      level: 'Developing',
+      description: 'Leveraged TypeScript for backend controllers of enterprise-level webapps and this personal website.'
     },
     {
       label: 'Angular 2+',
-      level: 'Intermediate',
-      description: 'angular2 experience'
+      level: 'Developing',
+      description: 'Worked within in an enterprise-level webapp utilizing Angular in the middle of migrating from AngularJS. Built multiple small projects with intention on becoming fluent in Angular.'
     },
     {
       label: 'AngularJS',
-      level: 'Intermediate',
-      description: 'angularjs experience'
+      level: 'Knowledgeable',
+      description: 'Worked with AngularJS functionality both on an enterprise webapp and within the ServiceNow service portal. Assisted in migrating pieces of webapp built in AngularJS to Angular2+.'
     },
     {
       label: 'ServiceNow',
-      level: 'Intermediate',
-      description: 'servicenow experience'
+      level: 'Knowledgeable',
+      description: 'Assisted in the development of a custom-built webapp frontend using the ServiceNow service portal, and leveraging core SerivceNow functionality for backend.'
     },
     {
       label: 'Java & SpringBoot',
-      level: 'Beginner',
-      description: 'java & springboot experience'
+      level: 'Developing',
+      description: 'Worked within multiple enterprise-level applications utilizing Java and Springboot backend. Used Java and built basic API endpoints with Spring for personal use cases.'
     },
     {
       label: 'SQL',
-      level: 'Beginner',
-      description: 'sql experience (raw sql, postgres, snowflake)'
+      level: 'Developing',
+      description: 'Familiar with crafting SQL queries for basic use cases and have utilized platforms such as Snowflake and dbt in an enterprise environment.'
     },
   ];
   selectedSkill: SkillItem = this.skills[0];
