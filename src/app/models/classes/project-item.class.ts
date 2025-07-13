d6610b91cc65290558942a42bdcca6c838a802bf
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-07-12 20:11:24 -0400

    Games page work

--- src/app/models/classes/project-item.class.ts
+++ src/app/models/classes/project-item.class.ts
@@ -8,19 +8,25 @@ export class ProjectItem {
     title:string;
     /** Description of the project */
     description:string;
+    /** HTML text body for display on the project detail page */
+    pageBody:string;
     /** The type of project this is (game or code)  */
     type:string;
+    /** Extra information specific to the project type (genre for games, tech stack for webdev, etc) */
+    meta:string;
     /** Array of media attributed to the project */
-    content:Array<GalleryItemContent>;
+    mediaContent:Array<GalleryItemContent>;
     /** Date the project was posted */
     dateCreated:Date;
 
-    constructor(idIn:string, titleIn:string, descriptionIn:string, typeIn:string, content:Array<GalleryItemContent>, dateCreatedIn:Date){
+    constructor(idIn:string, titleIn:string, descriptionIn:string, pageBodyIn:string, typeIn:string, metaIn:string, mediaContentIn:Array<GalleryItemContent>, dateCreatedIn:Date){
         this.id = idIn;
         this.title = titleIn;
         this.description = descriptionIn;
+        this.pageBody = pageBodyIn;
         this.type = typeIn;
-        this.content = content;
+        this.meta = metaIn;
+        this.mediaContent = mediaContentIn;
         this.dateCreated = dateCreatedIn;
     }
 }
