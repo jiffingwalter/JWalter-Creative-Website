9e4b8f26c36d1fcdb2ee6cbdef42b83da68a2eb1
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-07-12 23:34:27 -0400

    Implemented old avatar creator temporarily (with some tweaks to use angular)

--- src/app/components/pages/art-avatarcreator/art-avatarcreator.component.ts
+++ src/app/components/pages/art-avatarcreator/art-avatarcreator.component.ts
@@ -8,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrl: './art-avatarcreator.component.css'
 })
 export class ArtAvatarcreatorComponent {
+  headOptions:Array<string> = [
+    'Basic',
+    'ChinPointy',
+    'ChinRounded',
+    'ChinBold'
+  ];
+  hairOptions:Array<string> = [
+    'Bald',
+    'Layedover',
+    'StraightShort',
+    'StraightLong',
+    'StraightBun',
+    'WaveyShort',
+    'CurlyShort',
+    'PoofyShort',
+    'PoofyBig',
+    'Bowlcut',
+    'Spiked',
+    'SpikedStraight',
+    'SpikedLong',
+    'Mohawk'
+  ];
+  eyesOptions:Array<string> = [
+    'Basic',
+    'Half',
+    'Mad',
+    'Sad',
+    'Confused',
+    'Wide',
+    'Tiny',
+    'Dots',
+    'Weird',
+    'Anime'
+  ];
+  noseOptions:Array<string> = [
+    'Basic',
+    'Big',
+    'Small',
+    'Tiny',
+    'Wide',
+    'Angled',
+    'Pointy',
+  ];
+  mouthOptions:Array<string> = [
+    'Neutral',
+    'SmileSmall',
+    'SmileBig',
+    'SmileToothy',
+    'FrownSmall',
+    'FrownBig',
+    'FrownToothy',
+    'Slanted',
+  ];
 
+  selectedHead:string = this.headOptions[0];
+  selectedHair:string = this.hairOptions[0];
+  selectedEyes:string = this.eyesOptions[0];
+  selectedNose:string = this.noseOptions[0];
+  selectedMouth:string = this.mouthOptions[0];
 }
