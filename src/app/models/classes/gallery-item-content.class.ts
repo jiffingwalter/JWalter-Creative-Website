5d914c919c32a9629714b8b72a415ccd5d5511a3
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-07-16 21:30:14 -0400

    Added changes to content interface to mock json files

--- src/app/models/classes/gallery-item-content.class.ts
+++ src/app/models/classes/gallery-item-content.class.ts
@@ -3,20 +3,21 @@ export class GalleryItemContent{
     id:string; 
     /** Type of media of the content (image | video | audio) */
     type:string;
-    //galleryItemId:string;
+    /** The ID of the item this content is associated with */
+    parentId:string;
     /** Title of the content, used for alt text */
     title:string;
     /** File extention associated to the media */
     extention:string;
     /** Date the content was loaded into the database */
-    //loadDate:Date;
+    loadDate:Date;
 
-    constructor(idIn:string,typeIn:string,galleryItemIdIn:string,titleIn:string,extentionIn:string,loadDateIn:Date){
+    constructor(idIn:string,typeIn:string,parentId:string,titleIn:string,extentionIn:string,loadDateIn:Date){
         this.id = idIn;
         this.type = typeIn;
-        //this.galleryItemId = galleryItemIdIn;
+        this.parentId = parentId;
         this.extention = extentionIn;
         this.title = titleIn;
-        //this.loadDate = loadDateIn;
+        this.loadDate = loadDateIn;
     }
 }
